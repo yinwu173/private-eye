582475a27101e6b13217a3bc089f7851d9266427
yinwu173 <yin.wu173@gmail.com>
2024-09-17 22:05:11 -0700

    created table for role in schema.sql file

--- db/schema.sql
+++ db/schema.sql
@@ -6,5 +6,12 @@ CREATE DATABASE employeeTable_db;
 
 CREATE TABLE department (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(30) UNIQUE NOT NULL
+    name VARCHAR(30) UNIQUE NOT NULL -- to hold department name
+)
+
+CREATE TABLE role (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(30) UNIQUE NOT NULL, -- to hold role title
+    salary DECIMAL NOT NULL, -- to hold role salary
+    department_id INTEGER NOT NULL
 )
