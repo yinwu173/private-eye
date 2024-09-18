737263db700fd22e9c0105ac46312c5700c7dc06
yinwu173 <yin.wu173@gmail.com>
2024-09-17 22:13:22 -0700

    edited schema.sql: created table for employee

--- db/schema.sql
+++ db/schema.sql
@@ -15,3 +15,13 @@ CREATE TABLE role (
     salary DECIMAL NOT NULL, -- to hold role salary
     department_id INTEGER NOT NULL
 )
+
+CREATE TABLE employee (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER NOT NULL
+    manager_id INTEGER NOT NULL
+    FOREIGN KEY (role_id) REFERENCES roles(role_id), 
+    FOREIGN KEY (manager_id) REFERENCES managers(manager_id)
+)
