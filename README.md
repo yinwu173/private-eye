3679b7854c12821a8487836aa7191240fb7e289a
yinwu173 <yin.wu173@gmail.com>
2024-09-18 23:05:47 -0700

    editer server.js: used a switch statement so when user selects a choice, the command line will call to that choice and execute the function

---
+++ README.md
@@ -0,0 +1,13 @@
+# Employee Tracker 
+
+## About the Project
+
+*
+
+## Acceptance Criteria
+
+*
+
+### Resources
+
+* [Switch Statement](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/switch)
--- server.js
+++ server.js
@@ -23,5 +23,29 @@ inquirer
             ]
         }
     ])
-    .then()
+    .then((track) => {
+        switch (track) {
+            case 'View all departments':
+                viewAllDepartments();
+                break;
+            case 'View all roles':
+                viewAllRoles();
+                break;
+            case 'View all employees':
+                viewAllEmployees();
+                break;
+            case 'Add a department':
+                addDepartment();
+                break;
+            case 'Add a role':
+                addRole();
+                break;
+            case 'Add an employee':
+                addEmployee();
+                break;
+            case 'Update an employee role':
+                updateEmployeeRole();
+                break;
+        }
+    })
 // When chose to view all departments, I am presented with a formatted table showing department names and department ids
