a037322a027bd1330b93c274019aa16e247db65e
yinwu173 <yin.wu173@gmail.com>
2024-09-22 21:17:48 -0700

    edited server.js: added function to add department. Used inquirer prompt to prompt the user to add department name. Edited readme

--- README.md
+++ README.md
@@ -11,3 +11,5 @@
 ### Resources
 
 * [Switch Statement](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/switch)
+
+* [Creating Prompts](https://www.digitalocean.com/community/tutorials/nodejs-interactive-command-line-prompts)
--- server.js
+++ server.js
@@ -95,3 +95,34 @@ function viewAllEmployees() {
 };
 
 // WHEN I choose to add a department THEN I am prompted to enter the name of the department and that department is added to the database
+function addDepartment() {
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'departmentName',
+                message: 'Please enter name of the department:'
+            }
+        ])
+        .then((answer) => {
+            const sql = `INSERT INTO department (name)
+            VALUES (${departmentName})`;
+
+            connection.query(sql, answer.departmentName, (err) => {
+                if (err) {
+                    console.error('Error adding department: ', err);
+                }
+                console.log(`Department '${answer.departmentName}' added.`);
+            });
+        });
+};
+
+// WHEN I choose to add a role THEN I am prompted to enter the name, salary, and department for the role and that role is added to the database
+
+
+
+// WHEN I choose to add an employee THEN I am prompted to enter the employee’s first name, last name, role, and manager, and that employee is added to the database
+
+
+
+// WHEN I choose to update an employee role THEN I am prompted to select an employee to update and their new role and this information is updated in the database
